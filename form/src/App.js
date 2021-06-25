690833b36a50ad2ceadd255c530a35fd6d6f2332
aditi-1400 <aditisingh1400@gmail.com>
2021-06-25 18:15:25 +0530

    Added CPU and WiFi Data

--- form/src/App.js
+++ form/src/App.js
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { render } from "react-dom";
 import Form from "@rjsf/bootstrap-4";
 import Modal from "react-bootstrap/Modal";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Select from 'react-select';
+import Select from "react-select";
+
 const yaml = require("js-yaml");
 
 function App() {
+
+  function useForceUpdate(){
+    const [value, setValue] = React.useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
   const [isOpen, setIsOpen] = React.useState(false);
   const [content, setContent] = React.useState("Transitioning...");
-  const [formData, setFormData] = React.useState({});
+  const [formData, setFormData] = React.useState();
 
-
-
-  const CustomSelect = function(props){
-    
-    return(
-      <Select id="input"
-      options={props.options.enumOptions}
-      onChange={(e) => console.log(e)}
-      >
-      </Select>
-    )
-  }
+  const wifis = require("./wifis.json");
+  const cpus = require("./cpus.json")
+  const CustomSelect = function (props) {
+    return (
+      <Select
+        id="input"
+        className="basic-single"
+        classNamePrefix="select"
+      
+        options={props.options.enumOptions}
+        placeholder={props.label}
+        isSearchable={true}
+        
+        onChange={async (e) => {
+          await props.onChange(e.value);
+        }}
+      ></Select>
+    );
+  };
   const widgets = {
-    SelectWidget: CustomSelect
-  }
-
+    SelectWidget: CustomSelect,
+  };
 
   const showModal = () => {
     setIsOpen(true);
@@ -42,20 +54,38 @@ function App() {
     setContent(yaml.safeDump(formData, { skipInvalid: true }));
     showModal();
   };
-
-
-  fetch("schema.json",      { headers : { 
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-   }})
+  const forceUpdate = useForceUpdate();
+  fetch("schema.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
     .then((response) => response.json())
     .then((schema) => {
       render(
         <>
-          <Form schema={schema} 
-                widgets={widgets}  
-                onSubmit={onSubmit} 
-                formData={formData} />
+          <Form
+            key={formData}
+            schema={schema}
+            onSubmit={onSubmit}
+            liveValidate={true}
+            formData={formData}
+            widgets={widgets}
+            onChange={async (e) => {
+              
+              var fd = e.formData
+              if(fd.cpu.model){
+                var model = fd.cpu.model
+                
+                fd.cpu["cores"] = cpus[model].cores
+                fd.cpu["mhz"] = cpus[model].mhz
+              }
+              setFormData(fd);
+              console.log(fd, e.formData)
+              forceUpdate()
+            }}
+          />
         </>,
         document.getElementById("form")
       );
@@ -68,9 +98,7 @@ function App() {
           <Nav.Link href="https://github.com/aparcar/devices/tree/main/form">
             Source Code
           </Nav.Link>
-          <Nav.Link href="schema_doc.html">
-            Schema
-          </Nav.Link>
+          <Nav.Link href="schema_doc.html">Schema</Nav.Link>
         </Nav>
       </Navbar>
       <div id="form">
