74e49d08750332bd6771ed0faf445ecc34b61179
aditi-1400 <aditisingh1400@gmail.com>
2021-07-01 15:34:52 +0530

    Added the download file button and dropdown in cpu and wifi schemas

--- form/src/App.js
+++ form/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { render } from "react-dom";
 import Form from "@rjsf/bootstrap-4";
 import Modal from "react-bootstrap/Modal";
@@ -6,14 +6,36 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Select from "react-select";
 import logo from "./images/logo.png";
-import "./App.css"
+import './App.css'
 
 const yaml = require("js-yaml");
 
 function App() {
+
+  //document.getElementById
   const [isOpen, setIsOpen] = React.useState(false);
   const [content, setContent] = React.useState("Transitioning...");
-  const [formData, setFormData] = React.useState({});
+  const [formData, setFormData] = React.useState({"vendor":""});
+  const [url, setUrl] = React.useState("");
+  useEffect(() =>{
+    var data = new Blob([content]);
+    setUrl(URL.createObjectURL(data));
+  },[content])
+
+  const CustomSelect = function (props) {
+    return (
+      <Select
+        options={props.options.enumOptions}  
+        placeholder={props.label}
+        onChange={(e)=>{props.onChange(e.value)}}
+
+      ></Select>
+    );
+  };
+
+  const widgets = {
+    SelectWidget: CustomSelect,
+  };
 
   const showModal = () => {
     setIsOpen(true);
@@ -23,33 +45,21 @@ function App() {
     setIsOpen(false);
   };
 
+  const download = () =>{
+    var data = new Blob([content]);
+    setUrl(URL.createObjectURL(data));
+    var element = document.createElement('a');
+    element.setAttribute('href',url);
+    element.setAttribute('download',"device.yaml");
+    document.body.appendChild(element);
+    element.click();
+  }
+
   const onSubmit = ({ formData }, e) => {
     setFormData(formData);
     setContent(yaml.safeDump(formData, { skipInvalid: true }));
     showModal();
   };
-
-  const CustomSelect = function (props) {
-    
-    return (
-      <Select
-        id="input"
-        className="basic-single"
-        classNamePrefix="select"
-        options={props.options.enumOptions}
-        placeholder={props.label}
-        isSearchable={true}
-        onChange={async (e) => {
-          await props.onChange(e.value);
-        }}
-      ></Select>
-    );
-  };
-  
-  const widgets = {
-    SelectWidget: CustomSelect,
-  };
-
  
   fetch("schema.json", {
     headers: {
@@ -61,7 +71,19 @@ function App() {
     .then((schema) => {
       render(
         <>
-          <Form schema={schema} onSubmit={onSubmit} formData={formData} widgets={widgets}/>
+          <Form 
+            
+            key={formData}
+            schema={schema}
+            onSubmit={onSubmit}
+            formData={formData}
+            onChange={async (e)=>{
+              await setFormData(e.formData);
+              setContent(yaml.safeDump(formData, { skipInvalid: true }));
+              console.log(content)
+              }}
+            widgets={widgets}
+          />
         </>,
         document.getElementById("form")
       );
@@ -69,8 +91,8 @@ function App() {
   return (
     <>
       <Navbar>
-      <Nav>
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" className="logo"></img></Navbar.Brand>
+        <Nav>
+          <Navbar.Brand href="#home"><img src={logo} className="logo" alt="logo"></img></Navbar.Brand>
         </Nav>
         <Nav className="mr-0" >
           <Nav.Link href="https://github.com/aparcar/devices/tree/main/form">
@@ -79,8 +101,12 @@ function App() {
           <Nav.Link href="schema_doc.html">Schema</Nav.Link>
         </Nav>
       </Navbar>
-      <div id="form" className="container form">
+      <div className="container">
+      <div className="form" id="form">
         <p> Loading schemas...</p>
+        
+      </div>
+      <button onClick={download} className="download btn btn-primary">Download Data</button>
       </div>
       <Modal show={isOpen} onHide={hideModal}>
         <Modal.Header>
@@ -90,7 +116,7 @@ function App() {
           <pre>{content}</pre>
         </Modal.Body>
         <Modal.Footer>
-        <p className="button" onClick={hideModal}>Cancel</p> <p className="button" href="#" download="Data.yaml" type="text/yaml">Save</p>
+          <p className="button" onClick={hideModal}>Cancel</p> <a className="button" href={url} download="Data.yaml" type="text/yaml">Save</a>
         </Modal.Footer>
       </Modal>
     </>
@@ -98,3 +124,4 @@ function App() {
 }
 
 export default App;
+
